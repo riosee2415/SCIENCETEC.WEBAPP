0a646935b71d6ad186a463eac7820fb10bc253de
4leaf.peb <4leaf.peb@gmail.com>
2022-11-24 15:49:32 +0900

    modfiy

---
+++ front/pages/info/inquire.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  KAKAO_LOGIN_REQUEST,
+  LOAD_MY_INFO_REQUEST,
+  LOGIN_REQUEST,
+} from "../../reducers/user";
+import useInput from "../../hooks/useInput";
+import ClientLayout from "../../components/ClientLayout";
+import axios from "axios";
+import wrapper from "../../store/configureStore";
+import { END } from "redux-saga";
+import {
+  ColWrapper,
+  RowWrapper,
+  Image,
+  WholeWrapper,
+  Wrapper,
+  RsWrapper,
+} from "../../components/commonComponents";
+import useWidth from "../../hooks/useWidth";
+import Theme from "../../components/Theme";
+import styled from "styled-components";
+import Head from "next/head";
+import Popup from "../../components/popup/popup";
+import Mainslider from "../../components/slide/MainSlider";
+import ToastEditorComponent from "../../components/editor/ToastEditorComponent";
+import CC01 from "../../components/common/CC01";
+
+const Inquire = ({}) => {
+  const width = useWidth();
+  ////// GLOBAL STATE //////
+
+  ////// HOOKS //////
+  ////// REDUX //////
+  ////// USEEFFECT //////
+  ////// TOGGLE //////
+  ////// HANDLER //////
+  ////// DATAVIEW //////
+
+  return (
+    <>
+      <Head>
+        <title>ALAL</title>
+      </Head>
+
+      <ClientLayout>
+        <WholeWrapper>
+          <RsWrapper>
+            <Wrapper dr={`row`}>
+              <Wrapper
+                width={`240px`}
+                bgColor={Theme.basicTheme_C}
+                height={`300px`}
+              ></Wrapper>
+
+              <Wrapper
+                width={`calc(100% - 240px)`}
+                padding={`0 0 0 50px`}
+              ></Wrapper>
+            </Wrapper>
+          </RsWrapper>
+        </WholeWrapper>
+      </ClientLayout>
+    </>
+  );
+};
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  async (context) => {
+    // SSR Cookie Settings For Data Load/////////////////////////////////////
+    const cookie = context.req ? context.req.headers.cookie : "";
+    axios.defaults.headers.Cookie = "";
+    if (context.req && cookie) {
+      axios.defaults.headers.Cookie = cookie;
+    }
+    ////////////////////////////////////////////////////////////////////////
+    // 구현부
+
+    context.store.dispatch({
+      type: LOAD_MY_INFO_REQUEST,
+    });
+
+    // 구현부 종료
+    context.store.dispatch(END);
+    console.log("🍀 SERVER SIDE PROPS END");
+    await context.store.sagaTask.toPromise();
+  }
+);
+export default Inquire;
