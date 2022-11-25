dca541ac0d14434bace5e68e287a8844779cc06e
khj6407 <khj64076407@gmail.com>
2022-11-25 14:17:28 +0900

    commit

---
+++ front/pages/operate/community.jsx
@@ -0,0 +1,166 @@
+import React from "react";
+import ClientLayout from "../../components/ClientLayout";
+import Head from "next/head";
+import wrapper from "../../store/configureStore";
+import { LOAD_MY_INFO_REQUEST } from "../../reducers/user";
+import axios from "axios";
+import { END } from "redux-saga";
+import { useSelector } from "react-redux";
+import {
+  CommonButton,
+  CustomPage,
+  CustomSelect,
+  Image,
+  RsWrapper,
+  Text,
+  TextInput,
+  WholeWrapper,
+  Wrapper,
+} from "../../components/commonComponents";
+import LeftMenu from "../../components/LeftMenu";
+import BreadCrumb from "../../components/BreadCrumb";
+import styled from "styled-components";
+import useWidth from "../../hooks/useWidth";
+import Theme from "../../components/Theme";
+import { Select } from "antd";
+
+const Community = () => {
+  ////// GLOBAL STATE //////
+  ////// HOOKS //////
+  const width = useWidth();
+  ////// REDUX //////
+  ////// USEEFFECT //////
+  ////// TOGGLE //////
+  ////// HANDLER //////
+  ////// DATAVIEW //////
+
+  return (
+    <>
+      <Head>
+        <title>iCAST | 커뮤니티</title>
+      </Head>
+
+      <ClientLayout>
+        <WholeWrapper>
+          <RsWrapper dr={`row`} al={`flex-start`} position={`relative`}>
+            <LeftMenu />
+            <Wrapper width={width < 1100 ? `100%` : `calc(100% - 280px)`}>
+              <BreadCrumb />
+              <Wrapper
+                wrap={`nowrap`}
+                dr={`row`}
+                ju={`flex-start`}
+                fontSize={width < 900 ? `17px` : `20px`}
+                fontWeight={`700`}
+              >
+                <Image
+                  alt="icon"
+                  src={`https://4leaf-s3.s3.ap-northeast-2.amazonaws.com/sciencetec/assets/images/icon/title_circle.png`}
+                  width={`14px`}
+                  margin={`0 6px 0 0`}
+                />
+                기관형 과학기술인 협동조합 커뮤니티 자유게시판
+              </Wrapper>
+              <Wrapper dr={`row`} ju={`flex-start`} margin={`30px 0 20px`}>
+                <CustomSelect width={`90px`}>
+                  <Select placeholder="전체">
+                    <Select.Option>전체</Select.Option>
+                    <Select.Option></Select.Option>
+                  </Select>
+                </CustomSelect>
+                <TextInput
+                  type="text"
+                  placeholder="검색어를 입력해주세요."
+                  width={width < 900 ? `150px` : `230px`}
+                  height={`40px`}
+                  margin={`0 10px`}
+                />
+                <CommonButton width={`90px`} height={`40px`} fontSize={`16px`}>
+                  검색하기
+                </CommonButton>
+              </Wrapper>
+
+              <Wrapper
+                borderTop={`2px solid ${Theme.basicTheme_C}`}
+                height={`48px`}
+                bgColor={Theme.subTheme4_C}
+                borderBottom={`1px solid ${Theme.lightGrey2_C}`}
+                dr={`row`}
+              >
+                <Wrapper width={`8%`} display={width < 900 ? `none` : `flex`}>
+                  번호
+                </Wrapper>
+                <Wrapper width={`62%`}>
+                  <Text isNeo>제목</Text>
+                </Wrapper>
+                <Wrapper width={width < 900 ? `19%` : `15%`}>
+                  <Text isNeo>일시</Text>
+                </Wrapper>
+                <Wrapper width={width < 900 ? `19%` : `15%`}>
+                  <Text isNeo>게시자</Text>
+                </Wrapper>
+              </Wrapper>
+
+              <Wrapper
+                height={`48px`}
+                borderBottom={`1px solid ${Theme.lightGrey2_C}`}
+                dr={`row`}
+                fontSize={width < 900 ? `12px` : `14px`}
+              >
+                <Wrapper width={`8%`} display={width < 900 ? `none` : `flex`}>
+                  10
+                </Wrapper>
+                <Wrapper width={`62%`}>
+                  <Text width={`90%`} isEllipsis>
+                    설립 어떻게 해요?
+                  </Text>
+                </Wrapper>
+                <Wrapper width={width < 900 ? `19%` : `15%`}>
+                  2022.10.13
+                </Wrapper>
+                <Wrapper width={width < 900 ? `19%` : `15%`}>Science</Wrapper>
+              </Wrapper>
+
+              <Wrapper al={`flex-end`} margin={`30px 0 20px`}>
+                <CommonButton
+                  kindOf={`subTheme`}
+                  width={width < 900 ? `140px` : `160px`}
+                  height={width < 900 ? `45px` : `55px`}
+                  fontSize={width < 900 ? `16px` : `18px`}
+                  fontWeight={`bold`}
+                >
+                  작성하기
+                </CommonButton>
+              </Wrapper>
+              <CustomPage />
+            </Wrapper>
+          </RsWrapper>
+        </WholeWrapper>
+      </ClientLayout>
+    </>
+  );
+};
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  async (context) => {
+    // SSR Cookie Settings For Data Load/////////////////////////////////////
+    const cookie = context.req ? context.req.headers.cookie : "";
+    axios.defaults.headers.Cookie = "";
+    if (context.req && cookie) {
+      axios.defaults.headers.Cookie = cookie;
+    }
+    ////////////////////////////////////////////////////////////////////////
+    // 구현부
+
+    context.store.dispatch({
+      type: LOAD_MY_INFO_REQUEST,
+    });
+
+    // 구현부 종료
+    context.store.dispatch(END);
+    console.log("🍀 SERVER SIDE PROPS END");
+    await context.store.sagaTask.toPromise();
+  }
+);
+
+export default Community;
---
+++ front/pages/operate/notice.jsx
@@ -0,0 +1,166 @@
+import React from "react";
+import ClientLayout from "../../components/ClientLayout";
+import Head from "next/head";
+import wrapper from "../../store/configureStore";
+import { LOAD_MY_INFO_REQUEST } from "../../reducers/user";
+import axios from "axios";
+import { END } from "redux-saga";
+import { useSelector } from "react-redux";
+import {
+  CommonButton,
+  CustomPage,
+  CustomSelect,
+  Image,
+  RsWrapper,
+  Text,
+  TextInput,
+  WholeWrapper,
+  Wrapper,
+} from "../../components/commonComponents";
+import LeftMenu from "../../components/LeftMenu";
+import BreadCrumb from "../../components/BreadCrumb";
+import styled from "styled-components";
+import useWidth from "../../hooks/useWidth";
+import Theme from "../../components/Theme";
+import { Select } from "antd";
+
+const Notice = () => {
+  ////// GLOBAL STATE //////
+  ////// HOOKS //////
+  const width = useWidth();
+  ////// REDUX //////
+  ////// USEEFFECT //////
+  ////// TOGGLE //////
+  ////// HANDLER //////
+  ////// DATAVIEW //////
+
+  return (
+    <>
+      <Head>
+        <title>iCAST | 공지사항</title>
+      </Head>
+
+      <ClientLayout>
+        <WholeWrapper>
+          <RsWrapper dr={`row`} al={`flex-start`} position={`relative`}>
+            <LeftMenu />
+            <Wrapper width={width < 1100 ? `100%` : `calc(100% - 280px)`}>
+              <BreadCrumb />
+              <Wrapper
+                wrap={`nowrap`}
+                dr={`row`}
+                ju={`flex-start`}
+                fontSize={width < 900 ? `17px` : `20px`}
+                fontWeight={`700`}
+              >
+                <Image
+                  alt="icon"
+                  src={`https://4leaf-s3.s3.ap-northeast-2.amazonaws.com/sciencetec/assets/images/icon/title_circle.png`}
+                  width={`14px`}
+                  margin={`0 6px 0 0`}
+                />
+                기관형 과학기술인 협동조합 공지사항
+              </Wrapper>
+              <Wrapper dr={`row`} ju={`flex-start`} margin={`30px 0 20px`}>
+                <CustomSelect width={`90px`}>
+                  <Select placeholder="전체">
+                    <Select.Option>전체</Select.Option>
+                    <Select.Option></Select.Option>
+                  </Select>
+                </CustomSelect>
+                <TextInput
+                  type="text"
+                  placeholder="검색어를 입력해주세요."
+                  width={width < 900 ? `150px` : `230px`}
+                  height={`40px`}
+                  margin={`0 10px`}
+                />
+                <CommonButton width={`90px`} height={`40px`} fontSize={`16px`}>
+                  검색하기
+                </CommonButton>
+              </Wrapper>
+
+              <Wrapper
+                borderTop={`2px solid ${Theme.basicTheme_C}`}
+                height={`48px`}
+                bgColor={Theme.subTheme4_C}
+                borderBottom={`1px solid ${Theme.lightGrey2_C}`}
+                dr={`row`}
+              >
+                <Wrapper width={`8%`} display={width < 900 ? `none` : `flex`}>
+                  번호
+                </Wrapper>
+                <Wrapper width={`62%`}>
+                  <Text isNeo>제목</Text>
+                </Wrapper>
+                <Wrapper width={width < 900 ? `19%` : `15%`}>
+                  <Text isNeo>일시</Text>
+                </Wrapper>
+                <Wrapper width={width < 900 ? `19%` : `15%`}>
+                  <Text isNeo>게시자</Text>
+                </Wrapper>
+              </Wrapper>
+
+              <Wrapper
+                height={`48px`}
+                borderBottom={`1px solid ${Theme.lightGrey2_C}`}
+                dr={`row`}
+                fontSize={width < 900 ? `12px` : `14px`}
+              >
+                <Wrapper width={`8%`} display={width < 900 ? `none` : `flex`}>
+                  10
+                </Wrapper>
+                <Wrapper width={`62%`}>
+                  <Text width={`90%`} isEllipsis>
+                    설립 어떻게 해요?
+                  </Text>
+                </Wrapper>
+                <Wrapper width={width < 900 ? `19%` : `15%`}>
+                  2022.10.13
+                </Wrapper>
+                <Wrapper width={width < 900 ? `19%` : `15%`}>Science</Wrapper>
+              </Wrapper>
+
+              <Wrapper al={`flex-end`} margin={`30px 0 20px`}>
+                <CommonButton
+                  kindOf={`subTheme`}
+                  width={width < 900 ? `140px` : `160px`}
+                  height={width < 900 ? `45px` : `55px`}
+                  fontSize={width < 900 ? `16px` : `18px`}
+                  fontWeight={`bold`}
+                >
+                  작성하기
+                </CommonButton>
+              </Wrapper>
+              <CustomPage />
+            </Wrapper>
+          </RsWrapper>
+        </WholeWrapper>
+      </ClientLayout>
+    </>
+  );
+};
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  async (context) => {
+    // SSR Cookie Settings For Data Load/////////////////////////////////////
+    const cookie = context.req ? context.req.headers.cookie : "";
+    axios.defaults.headers.Cookie = "";
+    if (context.req && cookie) {
+      axios.defaults.headers.Cookie = cookie;
+    }
+    ////////////////////////////////////////////////////////////////////////
+    // 구현부
+
+    context.store.dispatch({
+      type: LOAD_MY_INFO_REQUEST,
+    });
+
+    // 구현부 종료
+    context.store.dispatch(END);
+    console.log("🍀 SERVER SIDE PROPS END");
+    await context.store.sagaTask.toPromise();
+  }
+);
+
+export default Notice;
