cd176264cdb9e0ac855f7e7166a3c9d13d2a6b8d
khj6407 <khj64076407@gmail.com>
2022-11-25 17:34:45 +0900

    commit

--- front/pages/admin/info/association.jsx
+++ front/pages/admin/info/association.jsx
@@ -2,7 +2,7 @@ import React, { useCallback, useEffect, useRef, useState } from "react";
 import AdminLayout from "../../../components/AdminLayout";
 import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
-import { Popover } from "antd";
+import { Button, Form, Image, Input, Popover } from "antd";
 import { useRouter, withRouter } from "next/router";
 import wrapper from "../../../store/configureStore";
 import { END } from "redux-saga";
@@ -106,7 +106,7 @@ const Association = ({}) => {
         <RightOutlined />
         <Popover content={content}>
           <HomeText cur={true} margin={`3px 20px 0px 20px`}>
-            {level2}{" "}
+            {level2}
           </HomeText>
         </Popover>
       </Wrapper>
@@ -119,6 +119,130 @@ const Association = ({}) => {
             화면 가이드안내 문구를 입력하세요. (RED COLOR)
           </GuideLi>
         </GuideUl>
+      </Wrapper>
+
+      <Wrapper
+        dr={`row`}
+        ju={`space-between`}
+        al={`flex-start`}
+        padding={`20px 20px 50px`}
+      >
+        <Wrapper width={`49%`}>
+          <Wrapper
+            borderBottom={`2px solid ${Theme.basicTheme_C}`}
+            padding={`0 0 15px`}
+            al={`flex-start`}
+            fontSize={`18px`}
+            fontWeight={`700`}
+          >
+            기술융합협동조합
+          </Wrapper>
+          <Image
+            alt="image"
+            src={`https://4leaf-s3.s3.ap-northeast-2.amazonaws.com/sciencetec/assets/images/usercoop-page/img.png`}
+          />
+          <Wrapper
+            dr={`row`}
+            height={`55px`}
+            fontSize={`16px`}
+            padding={`20px 0 0`}
+            borderTop={`1px solid ${Theme.lightGrey2_C}`}
+          >
+            <Form
+              style={{ width: "100%" }}
+              labelCol={{ span: 4 }}
+              wrapperCol={{ span: 20 }}
+            >
+              <Form.Item label="대표자명" name="title">
+                <Input size="small" allowClear />
+              </Form.Item>
+
+              <Form.Item label="설립연도" name="content">
+                <Input size="small" allowClear />
+              </Form.Item>
+
+              <Form.Item label="직원수" name="link">
+                <Input size="small" allowClear />
+              </Form.Item>
+
+              <Form.Item label="업종" name="updator">
+                <Input size="small" allowClear readOnly />
+              </Form.Item>
+
+              <Form.Item label="주업무" name="updatedAt">
+                <Input size="small" allowClear readOnly />
+              </Form.Item>
+
+              <Form.Item label="링크" name="createdAt">
+                <Input size="small" allowClear readOnly />
+              </Form.Item>
+
+              <Wrapper al="flex-end" margin="0px 0px 20px 0px">
+                <Button type="primary" size="small" htmlType="submit">
+                  데이터 수정
+                </Button>
+              </Wrapper>
+            </Form>
+          </Wrapper>
+        </Wrapper>
+        <Wrapper width={`49%`}>
+          <Wrapper
+            borderBottom={`2px solid ${Theme.basicTheme_C}`}
+            padding={`0 0 15px`}
+            al={`flex-start`}
+            fontSize={`18px`}
+            fontWeight={`700`}
+          >
+            회원법인조합
+          </Wrapper>
+          <Image
+            alt="image"
+            src={`https://4leaf-s3.s3.ap-northeast-2.amazonaws.com/sciencetec/assets/images/usercoop-page/img.png`}
+          />
+          <Wrapper
+            dr={`row`}
+            height={`55px`}
+            fontSize={`16px`}
+            padding={`20px 0 0`}
+            borderTop={`1px solid ${Theme.lightGrey2_C}`}
+          >
+            <Form
+              style={{ width: "100%" }}
+              labelCol={{ span: 4 }}
+              wrapperCol={{ span: 20 }}
+            >
+              <Form.Item label="대표자명" name="title">
+                <Input size="small" allowClear />
+              </Form.Item>
+
+              <Form.Item label="설립연도" name="content">
+                <Input size="small" allowClear />
+              </Form.Item>
+
+              <Form.Item label="직원수" name="link">
+                <Input size="small" allowClear />
+              </Form.Item>
+
+              <Form.Item label="업종" name="updator">
+                <Input size="small" allowClear readOnly />
+              </Form.Item>
+
+              <Form.Item label="주업무" name="updatedAt">
+                <Input size="small" allowClear readOnly />
+              </Form.Item>
+
+              <Form.Item label="링크" name="createdAt">
+                <Input size="small" allowClear readOnly />
+              </Form.Item>
+
+              <Wrapper al="flex-end" margin="0px 0px 20px 0px">
+                <Button type="primary" size="small" htmlType="submit">
+                  데이터 수정
+                </Button>
+              </Wrapper>
+            </Form>
+          </Wrapper>
+        </Wrapper>
       </Wrapper>
     </AdminLayout>
   );
