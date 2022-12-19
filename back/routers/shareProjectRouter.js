83c81484f46a8efaa8343b0cb39438aa97f9bd6e
sts2003 <4leaf.sts@gmail.com>
2022-12-19 15:20:17 +0900

    modify

--- back/routers/shareProjectRouter.js
+++ back/routers/shareProjectRouter.js
@@ -176,6 +176,133 @@ SELECT	ROW_NUMBER() OVER(ORDER BY RAND())		      AS	num,
   }
 });
 
+router.post("/admin/list", async (req, res, next) => {
+  const { searchData, searchname } = req.body;
+
+  const _searchname = searchname ? searchname : ``;
+
+  let _searchData = ``;
+
+  switch (parseInt(searchData)) {
+    case 1:
+      _searchData = `AND (A.name RLIKE '^(ㄱ|ㄲ)' OR ( A.name >= '가' AND A.name < '나' ))`;
+      break;
+    case 2:
+      _searchData = `AND (A.name RLIKE '^ㄴ' OR ( A.name >= '나' AND A.name < '다' ))`;
+      break;
+    case 3:
+      _searchData = `AND (A.name RLIKE '^(ㄷ|ㄸ)' OR ( A.name >= '다' AND A.name < '라' ))`;
+      break;
+    case 4:
+      _searchData = `AND (A.name RLIKE '^ㄹ' OR ( A.name >= '라' AND A.name < '마' ))`;
+      break;
+    case 5:
+      _searchData = `AND (A.name RLIKE '^ㅁ' OR ( A.name >= '마' AND A.name < '바' ))`;
+      break;
+    case 6:
+      _searchData = `AND (A.name RLIKE '^(ㅂ|ㅃ)' OR ( A.name >= '바' AND A.name < '사' ))`;
+      break;
+    case 7:
+      _searchData = `AND (A.name RLIKE '^(ㅅ|ㅆ)' OR ( A.name >= '사' AND A.name < '아' ))`;
+      break;
+    case 8:
+      _searchData = `AND (A.name RLIKE '^ㅇ' OR ( A.name >= '아' AND A.name < '자' ))`;
+      break;
+    case 9:
+      _searchData = `AND (A.name RLIKE '^(ㅈ|ㅉ)' OR ( A.name >= '자' AND A.name < '차' ))`;
+      break;
+    case 10:
+      _searchData = `AND (A.name RLIKE '^ㅊ' OR ( A.name >= '차' AND A.name < '카' ))`;
+      break;
+    case 11:
+      _searchData = `AND (A.name RLIKE '^ㅋ' OR ( A.name >= '카' AND A.name < '타' ))`;
+      break;
+    case 12:
+      _searchData = `AND (A.name RLIKE '^ㅌ' OR ( A.name >= '타' AND A.name < '파' ))`;
+      break;
+    case 13:
+      _searchData = `AND (A.name RLIKE '^ㅍ' OR ( A.name >= '파' AND A.name < '하' ))`;
+      break;
+    case 14:
+      _searchData = `AND (A.name RLIKE '^ㅎ' OR ( A.name >= '하'))`;
+      break;
+
+    default:
+      break;
+  }
+
+  const selectQuery = `
+SELECT	ROW_NUMBER() OVER(ORDER BY A.createdAt)		      AS	num,
+        A.id,
+        A.type,
+        A.name,
+        A.imagePath,
+        A.link,
+        A.repreName,
+        A.estimateDate,
+        DATE_FORMAT(A.estimateDate, "%Y년 %m월 %d일")	    AS viewEstimateDate,
+        A.empCnt,
+        CONCAT(FORMAT(A.empCnt, 0), "명")			           AS viewEmpCnt,
+        A.jobType,
+        A.importantWork,
+        B.username 									                    AS updator,
+        A.createdAt,
+        A.updatedAt,
+        DATE_FORMAT(A.createdAt, "%Y년 %m월 %d일")				AS viewCreatedAt,
+  		  DATE_FORMAT(A.updatedAt, "%Y년 %m월 %d일")				AS viewUpdatedAt
+  FROM	shareProjects		A
+ INNER
+  JOIN	users				B
+    ON	A.updator = B.id
+ WHERE	A.isDelete = 0
+        ${_searchData !== `` ? _searchData : ``}
+   AND  A.name LIKE '%${_searchname}%'
+ ORDER	BY num DESC
+  `;
+
+  try {
+    const list = await models.sequelize.query(selectQuery);
+
+    const selectQuery2 = `
+  SELECT	ROW_NUMBER()	OVER(ORDER	BY RAND())				AS num,
+          A.id,
+          A.name,
+          A.imagePath,
+          A.link,
+          A.repreName,
+          A.estimateDate,
+          DATE_FORMAT(A.estimateDate, "%Y년 %m월 %d일")	    AS viewEstimateDate,
+          A.empCnt,
+          CONCAT(FORMAT(A.empCnt, 0), "명")			           AS viewEmpCnt,
+          A.jobType,
+          A.importantWork,
+          B.username 									          	        AS updator,
+          A.ShareProjectId,
+          A.createdAt,
+          A.updatedAt,
+          DATE_FORMAT(A.createdAt, "%Y년 %m월 %d일")				AS viewCreatedAt,
+          DATE_FORMAT(A.updatedAt, "%Y년 %m월 %d일")				AS viewUpdatedAt
+    FROM	underShareProjects		A
+   INNER
+    JOIN	users					        B
+      ON	A.updator = B.id
+   WHERE	A.isDelete = 0
+     AND  A.name LIKE '%${_searchname}%'
+   ORDER	BY num DESC
+  `;
+
+    const underList = await models.sequelize.query(selectQuery2);
+
+    return res.status(200).json({
+      share: list[0],
+      underList: underList[0],
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(401).send("공동 프로젝트 목록을 불러올 수 없습니다.");
+  }
+});
+
 router.post("/create", async (req, res, next) => {
   const insertQuery = `
   INSERT    INTO    shareProjects
@@ -234,7 +361,7 @@ router.post("/create", async (req, res, next) => {
     return res.status(201).json({ result: true });
   } catch (error) {
     console.error(error);
-    return res.status(401).send("공동 프로젝트 정보를 생성할 수 없습니다.");
+    return res.status(401).send("회원 조합 소개를 생성할 수 없습니다.");
   }
 });
 
@@ -291,7 +418,45 @@ router.post("/update", async (req, res, next) => {
     return res.status(200).json({ result: true });
   } catch (error) {
     console.error(error);
-    return res.status(401).send("공동 프로젝트 정보를 수정할 수 없습니다.");
+    return res.status(401).send("회원 조합 소개를 수정할 수 없습니다.");
+  }
+});
+
+router.post("/image/update", isAdminCheck, async (req, res, next) => {
+  const { id, imagePath } = req.body;
+
+  const updateQuery = `
+  UPDATE  shareProjects
+     SET  imagePath = "${imagePath}",
+          updatedAt = NOW()
+   WHERE  id = ${id}
+  `;
+
+  const historyInsertQuery = `
+  INSERT INTO shareProjectHistory
+  (
+    value,
+    updator,
+    createdAt,
+    updatedAt
+  )
+  VALUES 
+  (
+    "회원 조합 이미지 수정",
+    ${req.user.id},
+    now(),
+    now()
+  )
+  `;
+
+  try {
+    await models.sequelize.query(updateQuery);
+    await models.sequelize.query(historyInsertQuery);
+
+    return res.status(200).json({ result: true });
+  } catch (error) {
+    console.error(error);
+    return res.status(401).send("조합 이미지를 수정할 수 없습니다.");
   }
 });
 
@@ -329,7 +494,7 @@ router.post("/delete", async (req, res, next) => {
     return res.status(200).json({ result: true });
   } catch (error) {
     console.error(error);
-    return res.status(401).send("공동 프로젝트 정보를 삭제할 수 없습니다.");
+    return res.status(401).send("회원 조합 소개를 삭제할 수 없습니다.");
   }
 });
 
@@ -558,6 +723,44 @@ router.post("/under/update", isAdminCheck, async (req, res, next) => {
   } catch (error) {
     console.error(error);
     return res.status(401).send("산하 조합 정보를 수정할 수 없습니다.");
+  }
+});
+
+router.post("/under/image/update", isAdminCheck, async (req, res, next) => {
+  const { id, imagePath } = req.body;
+
+  const updateQuery = `
+  UPDATE  underShareProjects
+     SET  imagePath = "${imagePath}",
+          updatedAt = NOW()
+   WHERE  id = ${id}
+  `;
+
+  const historyInsertQuery = `
+  INSERT INTO shareProjectHistory
+  (
+    value,
+    updator,
+    createdAt,
+    updatedAt
+  )
+  VALUES 
+  (
+    "산하 조합 이미지 수정",
+    ${req.user.id},
+    now(),
+    now()
+  )
+  `;
+
+  try {
+    await models.sequelize.query(updateQuery);
+    await models.sequelize.query(historyInsertQuery);
+
+    return res.status(200).json({ result: true });
+  } catch (error) {
+    console.error(error);
+    return res.status(401).send("산하 조합 이미지를 수정할 수 없습니다.");
   }
 });
 
